08d1b24da598436c7265d276c155b7e53868cef6
jkimel <jkimel@mail.austral.edu.ar>
2025-03-17 12:10:57 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,10 @@
 def math():
     a = 57
-    b = 7
+    b = 7   
+    print('El resultado de la suma es:',a+b)
+    print('El resultado de la resta es:',a-b)
+    print('El resultado de la multiplicacion es:',a*b)
+    print('El promedio es:',(a+b)/2)
+    print('El cociente entero es:',a//b)
+    print('El resto de la división entera es:',a%b)
+    print('El valor real de la división es:',a/b)
